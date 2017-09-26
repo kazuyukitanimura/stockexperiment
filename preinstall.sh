39c23e8b0ecd8af92c8e898d65fe47559ab10cc1
kazuyukitanimura <kazuyukitanimura@gmail.com>
2017-09-25 20:14:43 -0700

    comment out wget

--- preinstall.sh
+++ preinstall.sh
@@ -1,5 +1,5 @@
 rm -rf IBJts
-wget -c http://interactivebrokers.github.io/downloads/twsapi_macunix.972.18.zip
+#wget -c http://interactivebrokers.github.io/downloads/twsapi_macunix.972.18.zip
 unzip twsapi_macunix.972.18.zip
 rm -rf META-INF
 
