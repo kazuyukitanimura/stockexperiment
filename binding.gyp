8dd0f4210af67f3a59e330b574c183079f78100e
kazuyukitanimura <kazuyukitanimura@gmail.com>
2017-10-26 22:48:09 -0700

    inline

--- binding.gyp
+++ binding.gyp
@@ -14,15 +14,15 @@
       "include_dirs": [
         "IBJts/source/CppClient/client",
       ],
-      "cflags":  ['-c -O3 -ffast-math -fexpensive-optimizations -DNDEBUG -march=native -std=c++11'],
-      "cflags_cc": ['-c -O3 -ffast-math -fexpensive-optimizations -DNDEBUG -march=native -std=c++11'],
+      "cflags":  ['-ffast-math -fexpensive-optimizations -DNDEBUG -march=native -std=c++11'],
+      "cflags_cc": ['-ffast-math -fexpensive-optimizations -DNDEBUG -march=native -std=c++11'],
       'cflags!': [ '-fno-exceptions' ],
       'cflags_cc!': [ '-fno-exceptions' ],
       'conditions': [
         ['OS=="mac"', {
           'xcode_settings': {
             'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
-            'OTHER_CPLUSPLUSFLAGS': ['-std=c++11'],
+            'OTHER_CPLUSPLUSFLAGS': ['-ffast-math -fexpensive-optimizations -DNDEBUG -march=native -std=c++11'],
           },
         }],
       ]
--- preinstall.sh
+++ preinstall.sh
@@ -3,12 +3,18 @@ rm -rf IBJts
 unzip twsapi_macunix.972.18.zip
 rm -rf META-INF
 
+clientpath=IBJts/source/CppClient/client
 # http://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
-sed -i.bak 's/assert(dynamic_cast/\/\/assert(dynamic_cast/' IBJts/source/CppClient/client/EClientSocket.cpp
-sed -i.bak 's/min/std::min/' IBJts/source/CppClient/client/EReader.cpp
-sed -i.bak 's/TICK_SIZE:/TICK_SIZE:break;/' IBJts/source/CppClient/client/EDecoder.cpp
-sed -i.bak 's/TICK_OPTION_COMPUTATION:/TICK_OPTION_COMPUTATION:break;/' IBJts/source/CppClient/client/EDecoder.cpp
-sed -i.bak 's/TICK_GENERIC:/TICK_GENERIC:break;/' IBJts/source/CppClient/client/EDecoder.cpp
-sed -i.bak 's/TICK_STRING:/TICK_STRING:break;/' IBJts/source/CppClient/client/EDecoder.cpp
-sed -i.bak 's/TICK_EFP:/TICK_EFP:break;/' IBJts/source/CppClient/client/EDecoder.cpp
-sed -i.bak 's/m_pEWrapper->tickSize/;\/\/m_pEWrapper->tickSize/' IBJts/source/CppClient/client/EDecoder.cpp
+sed -i.bak 's/min/std::min/' ${clientpath}/EReader.cpp
+sed -i.bak 's/assert(dynamic_cast/\/\/assert(dynamic_cast/' ${clientpath}/EClientSocket.cpp
+sed -i.bak 's/TICK_SIZE:/TICK_SIZE:break;/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/TICK_OPTION_COMPUTATION:/TICK_OPTION_COMPUTATION:break;/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/TICK_GENERIC:/TICK_GENERIC:break;/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/TICK_STRING:/TICK_STRING:break;/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/TICK_EFP:/TICK_EFP:break;/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/m_pEWrapper->tickSize/;\/\/m_pEWrapper->tickSize/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/bool EDecoder::CheckOffset/inline bool EDecoder::CheckOffset/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/const char\* EDecoder::FindFieldEnd/inline const char\* EDecoder::FindFieldEnd/' ${clientpath}/EDecoder.cpp
+#sed -i.bak 's/bool EDecoder::DecodeField/inline bool EDecoder::DecodeField/' ${clientpath}/EDecoder.cpp
+sed -i.bak 's/bool EClient::isConnected/inline bool EClient::isConnected/' ${clientpath}/EClient.cpp
+#sed -i.bak 's/void EClient::EncodeField/inline void EClient::EncodeField/' ${clientpath}/EClient.cpp
